5cbefe18c8815efc983138390b31a641fd2e9075
Bryce Chen <Bryce1_Chen@pegatroncorp.com>
2018-02-01 14:47:43 +0800

    Updated the method for setting DBUS_SESSION_BUS_ADDRESS

--- setup.bash
+++ setup.bash
@@ -1,2 +1,6 @@
 #! /bin/bash
-export DBUS_SESSION_BUS_ADDRESS=$(ps ax | grep " dbus-daemon" | head -1 | awk -F'address=' '{ print $2 }')
+#export DBUS_SESSION_BUS_ADDRESS=$(ps ax | grep " dbus-daemon" | head -1 | awk -F'address=' '{ print $2 }')
+if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+	eval `cat $XDG_RUNTIME_DIR/dbus-session`
+	echo "D-Bus per-session daemon address is: $DBUS_SESSION_BUS_ADDRESS"
+fi
